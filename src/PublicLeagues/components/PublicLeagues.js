498bdb7e613faa1f27bf65652ffc8a9a2c3cf0fb
notmarkmiranda <notmarkmiranda@gmail.com>
2019-02-28 23:17:47 -0700

    setup styling on public leagues

--- src/PublicLeagues/components/PublicLeagues.js
+++ src/PublicLeagues/components/PublicLeagues.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import '../../Common/stylesheets/index.css'
 import { getPublicLeagues } from '../actions/index'
 
 export class PublicLeagues extends Component {
@@ -8,17 +9,50 @@ export class PublicLeagues extends Component {
     this.props.onGetPublicLeagues()
   }
 
+  renderLeagues = (leagues) => {
+    return leagues.map((league) => {
+      return (
+        <li className="list-group-item public-league" key={ league.id }>
+          { league.location }
+          <span className="stat-line">
+            { league.name }
+          </span>
+          <div className="caption-text text-info">
+            # of Games: { league.games_count } | Average Players per Game: { league.average_players_per_game } | Average Pot: { league.average_pot_size }
+          </div>
+        </li>
+      )
+    })
+  }
+
+  renderNoLeagues = () => {
+    return (
+      <li className="list-group-item public-leauge">
+        <span className="stat-line">
+          There are no leagues
+        </span>
+      </li>
+    )
+  }
+
   render() {
     const { publicLeagues } = this.props
-    console.log(this.props);
     return (
-      <div>
-      {
-        publicLeagues.length > 0 &&
-          publicLeagues.map(league => (
-            <div key={ league.id }>{ league.name }</div>
-          ))
-      }
+      <div className="d-flex justify-content-center">
+        <div className="col-lg-11">
+          <div className="card leagues mb-4 mt-4">
+            <div className="card-header header-text">
+              Public Leagues
+            </div>
+            <ul className="list-group list-group-flush leagues-list">
+            {
+              publicLeagues.length > 0
+                ? this.renderLeagues(publicLeagues)
+                : this.renderNoLeagues()
+            }
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
